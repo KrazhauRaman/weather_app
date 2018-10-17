dbfccf423942d727d15faa999e9de8f4b3133007
KrazhauRaman <krazhau.raman@gmail.com>
2018-10-17 22:09:14 +0300

    Updates

---
+++ precache-manifest.48b9f36e0f9e9b4dbfffb4e0f4fcffa2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b8e50d38204ea6f6971",
+    "url": "/weather_app/static/js/runtime~main.2b8e50d3.js"
+  },
+  {
+    "revision": "1ca5f15f365c2f1d6750",
+    "url": "/weather_app/static/js/main.1ca5f15f.chunk.js"
+  },
+  {
+    "revision": "5746d9bce76e8e091562",
+    "url": "/weather_app/static/js/1.5746d9bc.chunk.js"
+  },
+  {
+    "revision": "1ca5f15f365c2f1d6750",
+    "url": "/weather_app/static/css/main.36d6cb9f.chunk.css"
+  },
+  {
+    "revision": "5746d9bce76e8e091562",
+    "url": "/weather_app/static/css/1.398d0763.chunk.css"
+  },
+  {
+    "revision": "37929627acf71cc98f11892d87969e1a",
+    "url": "/weather_app/index.html"
+  }
+];
